e7f0de29bf5805bad6ebfa98f570227e05577177
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-20 14:48:47 +0300

    Initial Commit

---
+++ MiniGame/MiniGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniGameApp.swift
+//  MiniGame
+//
+//  Created by jopootrivatel on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MiniGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
